47a1ae70c215ec2a38758c2929d41dcbfb3601d4
Christian Peterek <christian.peterek@gmail.com>
2011-11-08 23:29:00 +0100

    added ImageReader Prototype... and the files

---
+++ DlImageParsr/ImageReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace DlImageParsr
+{
+    public class ImageReader
+    {
+        private Bitmap _image;
+        private Func<int, int, PixelType> getPixelFromImage;
+
+        public ImageReader(Bitmap image){
+            _image = image;
+
+            getPixelFromImage = (x, y) => (PixelType)_image.GetPixel(x, y).G;
+        }
+
+        public ImageReader(Func<int, int, PixelType> pixelReceiver) {
+            getPixelFromImage = pixelReceiver;
+        }
+
+        public int FrameX {get;set;}
+        public int FrameY {get;set;}
+        public int FrameWidth {get;set;}
+        public int FrameHeight {get;set;}
+ 
+        public int CurrentColumn{get;private set;}
+        public int CurrentRow{get;private set;}
+
+        public bool NextRow(){
+            return true;
+        }
+
+        public bool NextColumn(){
+            return true;
+        }
+
+        public PixelType CurrentPixel() {
+            return PixelType.undefined;
+        }
+    }
+}
